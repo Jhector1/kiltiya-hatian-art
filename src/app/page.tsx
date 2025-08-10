d42af5cfe065f14ee2a708818ba40d06e7fd5302
Jean Yves Hector <jeanyveshector@gmail.com>
2025-08-10 14:16:17 -0500

    new anitation added in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
-'use client';
+"use client";
 
 import AboutComponent from "@/components/AboutComponent";
 // import PngEditor from "@/components/PNGEditor";
 import SEO from "@/components/SEO";
+import VeveDrawFromSvg from "@/components/animation/VeveDrawFromSvg";
 // import SvgEditor from "@/components/SvgEditor";
 // import DigitalCard from "@/components/digitalCard/DigitalCard";
 // import dynamic from "next/dynamic";
@@ -23,30 +23,40 @@ import categories from "@/data/categories";
 // import ImageSlider from "@/components/product/ImageSlider";
 
 export default function Home() {
-
   return (
     <>
-    {/* <h1 className="brush-logo">Strain</h1> */}
+      {/* <h1 className="brush-logo">Strain</h1> */}
 
       <SEO
         title="Haitian Digital Art Gallery"
         description="Buy and explore uniquely crafted Haitian vector artworks."
       />
       <Hero />
-       {/* <ImageSlider images={images} />; */}
+      {/* <ImageSlider images={images} />; */}
       <PromoBanner />
-      <CategorySlider categories={categories} />
+      {/* <div className="flex justify-around w-[min(90vw,900px)]"> */}
+        <div className="m-auto pb-4 w-[min(90vw,900px)]">
+          <VeveDrawFromSvg
+            loop
+            src="/erzulie_freda.svg" // put your file in /public/veves/
+            sortByAttr="data-order" // optional: control draw sequence
+            delayStep={0.08}
+            duration={1.2}
+          />
+        </div>
+        <CategorySlider categories={categories} />
+      {/* </div> */}
       <CategoryGrid categories={categories} />
 
       <WhyHaitianArt />
-      <AboutComponent/>
+      <AboutComponent />
       <Testimonial />
 
       {/* <SvgEditor/>
       <PngEditor/>
   */}
+
       {/* <DigitalCardWrapper /> */}
-      
     </>
   );
 }
